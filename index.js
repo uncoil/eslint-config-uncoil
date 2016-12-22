b6df7b49fe0beee6e1774b8daa58b0d165c0e163
jakeorr <jake.a.orr@gmail.com>
2016-12-21 16:46:20 -0800

    Adding rules. Removing mocha env.

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 module.exports = {
-  "extends": "airbnb",
-  "plugins": [
-    "react"
-  ],
-  "rules": {
-    "no-console": "off",
-    "prefer-template": "warn",
-    "max-len": ["warn" , 100],
-    "no-multiple-empty-lines": ["error", {"max": 3, "maxEOF": 1}],
-    "no-param-reassign": ["error", { "props": true }],
-    "arrow-body-style": "off",
-    "padded-blocks": ["error", "never"],
-    "no-underscore-dangle": ["error", { "allowAfterThis": true }],
-    "object-shorthand": ["error", "properties", { "avoidQuotes": true }],
-    "react/jsx-uses-react": "error",
-    "react/jsx-uses-vars": "error",
-    "id-length": ["error", {"exceptions": ["n", "m", "i", "j", "k"]}]
+  extends: 'airbnb',
+  plugins: ['react'],
+  rules: {
+    'no-console': 'off',
+    'prefer-template': 'warn',
+    'max-len': ['warn' , 100],
+    'no-multiple-empty-lines': ['error', {max: 3, maxEOF: 1}],
+    'no-param-reassign': ['error', { props: true }],
+    'arrow-body-style': 'off',
+    'padded-blocks': ['error', 'never'],
+    'no-underscore-dangle': ['error', { allowAfterThis: true }],
+    'object-shorthand': ['error', 'properties', { avoidQuotes: true }],
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'id-length': ['error', {exceptions: ['n', 'm', 'i', 'j', 'k']}],
+    'comma-dangle': ['error', { functions: 'never' }],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
   },
-  "env": {
-    "node": true,
-    "mocha": true
-  }
-}
+  env: {
+    node: true,
+  },
+};
