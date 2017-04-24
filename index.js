2895812c0f76e42279c90c85b24f3261c311b0b4
jakeorr <jake.a.orr@gmail.com>
2017-04-24 15:48:17 -0700

    Updating for Prettier.

--- index.js
+++ index.js
@@ -9,21 +9,31 @@ module.exports = {
     'prefer-template': 'warn',
 
     // maximum line length
-    'max-len': ['warn', 100, {
-      'ignoreTrailingComments': true, // don't count trailing comments towards max-len.
-      'ignoreUrls': true, // don't count lines that contain a URL towards this
-    }],
+    'max-len': [
+      'warn',
+      100,
+      {
+        ignoreTrailingComments: true, // don't count trailing comments towards max-len.
+        ignoreUrls: true, // don't count lines that contain a URL towards this
+      },
+    ],
 
     // disallow multiple empty lines and only one newlinen at the end
-    'no-multiple-empty-lines': ['error', {
-      max: 3,
-      maxEOF: 1,
-    }],
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 3,
+        maxEOF: 1,
+      },
+    ],
 
     // disallow reassignment of function parameters (including props of parameters)
-    'no-param-reassign': ['error', {
-      props: true,
-    }],
+    'no-param-reassign': [
+      'error',
+      {
+        props: true,
+      },
+    ],
 
     // no checking for braces in arrow function body
     'arrow-body-style': 'off',
@@ -32,19 +42,29 @@ module.exports = {
     'padded-blocks': ['error', 'never'],
 
     // disallow dangling underscores in identifiers
-    'no-underscore-dangle': ['error', {
-      allowAfterThis: true,
-    }],
+    'no-underscore-dangle': [
+      'error',
+      {
+        allowAfterThis: true,
+      },
+    ],
 
     // require object literal shorthand
-    'object-shorthand': ['error', 'properties', {
-      avoidQuotes: true,
-    }],
+    'object-shorthand': [
+      'error',
+      'properties',
+      {
+        avoidQuotes: true,
+      },
+    ],
 
-    'no-plusplus': ['error', {
-      // e.g., allow for (let i = 0; i < 10; i++) but no other use of ++
-      allowForLoopAfterthoughts: true,
-    }],
+    'no-plusplus': [
+      'error',
+      {
+        // e.g., allow for (let i = 0; i < 10; i++) but no other use of ++
+        allowForLoopAfterthoughts: true,
+      },
+    ],
 
     // prevent React from being incorrectly marked as unused
     'react/jsx-uses-react': 'error',
@@ -53,39 +73,52 @@ module.exports = {
     'react/jsx-uses-vars': 'error',
 
     // require trailing commas in multiline
-    'comma-dangle': ['error', {
-      arrays: 'always-multiline',
-      objects: 'always-multiline',
-      imports: 'always-multiline',
-      exports: 'always-multiline',
-      functions: 'ignore',
-    }],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'ignore',
+      },
+    ],
 
     // allow either .js or .jsx for files containing JSX
-    'react/jsx-filename-extension': ['error', {
-      extensions: ['.js', '.jsx'],
-    }],
-   
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+
     // require properties to be on their own line iff there are multiple lines
     // allowMultiplePropertiesPerLine allows there to be EITHER all properties on one line, or all on their own line, but not both.
-    'object-property-newline': ['error', { 'allowMultiplePropertiesPerLine': true }],
+    'object-property-newline': ['error', { allowMultiplePropertiesPerLine: true }],
 
     // override of airbnb indent rule to enable MemberExpressions option
-    'indent': ['error', 2, {
-      'SwitchCase': 1,
-      'VariableDeclarator': 1,
-      'outerIIFEBody': 1,
-      // enforce indent for chained calls
-      'MemberExpression': 1,
-      'FunctionDeclaration': {
-        'parameters': 1,
-        'body': 1
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+        VariableDeclarator: 1,
+        outerIIFEBody: 1,
+        // enforce indent for chained calls
+        MemberExpression: 1,
+        FunctionDeclaration: {
+          parameters: 1,
+          body: 1,
+        },
+        FunctionExpression: {
+          parameters: 1,
+          body: 1,
+        },
       },
-      'FunctionExpression': {
-        'parameters': 1,
-        'body': 1
-      }
-    }]
+    ],
+
+    // Allows omiting parens for arrow functions when there is only one argument.
+    'arrow-parens': 'as-needed',
   },
   env: {
     node: true,
